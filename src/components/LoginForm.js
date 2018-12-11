23e6da16762c2ba308d2587048bf5218cd19b482
CarsonRey <c.rey494@gmail.com>
2018-12-11 09:43:05 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,23 +3,44 @@ import React from "react";
 class LoginForm extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
 
+
+  handleChange = (event) => {
+    this.setState({
+    [event.target.name]: event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    const username = this.state.username
+    const password = this.state.password
+    if (username !== '' && password !== ''){
+      this.props.onSubmit({username, password})
+    }
+  }
+
+
+
   render() {
+
     return (
-      <form>
+      <form onSubmit={this.onSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,17 +1,25 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+    value: ''
+    };
+  }
 
-    this.state = {};
+  handleChange = event => {
+    this.setState({
+      value: event.target.value
+    })
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" name="message" value={this.state.value} onChange={this.handleChange} />
+        <p>{this.props.maxChars - this.state.value.length}</p>
       </div>
     );
   }
